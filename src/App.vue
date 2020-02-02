6e9ddb2592ef3b8d19b83d1c4a4773363821a0a5
codeinsack <codeinsack@gmail.com>
2020-02-02 11:19:53 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>Server status: {{ status }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      status: 'Critical',
+    }
+  }
+}
+</script>
+
+<style>
+</style>
